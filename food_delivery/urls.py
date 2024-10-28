43078f474dfce37270ff98d875d1934e644551ec
ons2022 <onsismailhajrigmail.com>
2024-10-28 11:33:31 +0000

    first

--- food_delivery/urls.py
+++ food_delivery/urls.py
@@ -4,6 +4,8 @@ from rest_framework.routers import DefaultRouter
 from foods.views import FoodViewSet
 from users.views import register, login
 from orders.views import add_to_cart, place_order
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = DefaultRouter()
 router.register(r'foods', FoodViewSet)
@@ -15,4 +17,4 @@ urlpatterns = [
     path('api/cart/add/', add_to_cart),
     path('api/order/place/', place_order),
     path('api/', include(router.urls)),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- foods/serializers.py
+++ foods/serializers.py
@@ -5,3 +5,6 @@ class FoodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Food
         fields = ['id', 'name', 'description', 'price', 'image', 'available']
+def get_image_url(self, obj):
+        request = self.context.get('request')
+        return request.build_absolute_uri(obj.image.url) if obj.image else None
